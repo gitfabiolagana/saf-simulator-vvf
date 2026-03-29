c8c32a834b332546fc5c94c9d9677d6bd5aa4af0
Fabio Laganà <fabio.lagana@gmail.com>
2026-03-30 00:29:05 +0200

    Fix: base path for github pages and sw registration

--- src/main.jsx
+++ src/main.jsx
@@ -11,7 +11,7 @@ createRoot(document.getElementById('root')).render(
 
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/sw.js').then(reg => {
+    navigator.serviceWorker.register(`${import.meta.env.BASE_URL}sw.js`).then(reg => {
       console.log('SW registered!', reg);
     }).catch(err => {
       console.log('SW registration failed: ', err);
--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import path from "path"
 
 export default defineConfig({
   plugins: [react()],
+  base: "/saf-simulator-vvf/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
